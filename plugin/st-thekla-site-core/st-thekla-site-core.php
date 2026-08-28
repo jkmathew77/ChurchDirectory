92fe18720d463c48111b98d7ba079044e5dea306
John K. Mathew <51372339+jkmathew77@users.noreply.github.com>
2026-08-28 00:09:31 -0400

    Release Site Core 0.3.1 visual correction

--- plugin/st-thekla-site-core/st-thekla-site-core.php
+++ plugin/st-thekla-site-core/st-thekla-site-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: St. Thekla Site Core
  * Plugin URI:  https://www.sttheklachurch.org/
  * Description: Stable, church-owned public-site features for service schedules, leadership, announcements, contact details, and app integrations.
- * Version:     0.3.0
+ * Version:     0.3.1
  * Author:      St. Thekla Church
  * Author URI:  https://www.sttheklachurch.org/
  * Text Domain: st-thekla-site-core
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'STC_VERSION', '0.3.0' );
+define( 'STC_VERSION', '0.3.1' );
 define( 'STC_DATA_VERSION', '003' );
 define( 'STC_PLUGIN_FILE', __FILE__ );
 define( 'STC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
